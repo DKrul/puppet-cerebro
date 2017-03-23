106c64a5c5a24b21f53b586a8c7a2524f3b1b038
Fabian van der Hoeven <fabian.vanderhoeven@vermont24-7.com>
2017-03-23 08:43:51 +0100

    Added ability to install java in this module

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,25 @@ class cerebro (
   $package_url    = "https://github.com/lmenezes/cerebro/releases/download/v${version}/cerebro-${version}.zip",
   $service_ensure = 'running',
   $service_enable = true,
+  $java_install   = false,
+  $java_package   = undef,
 ) {
+  anchor {'cerebro::begin': }
 
   $cerebro_user = 'cerebro'
+
+  if $java_install == true {
+    # Install java
+    class { '::java':
+      package      => $java_package,
+      distribution => 'jre',
+    }
+
+    # ensure we first install java, the package and then the rest
+    Anchor['cerebro::begin']
+    -> Class['::java']
+    -> Class['cerebro::install']
+  }
 
   class { 'cerebro::user':
     user => $cerebro_user,
